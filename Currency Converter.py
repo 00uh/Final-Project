bef9473527e78e6df3b43c0d441bba3e364ba681
00uh <157229685+00uh@users.noreply.github.com>
2024-02-24 06:44:48 -0800

    Update Currency Converter.py

--- Currency Converter.py
+++ Currency Converter.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def currency_converter(amount, from_currency, to_currency):
     # Conversion rates (for simplicity, rates are hardcoded)
-    rates = {'USD': 1.0, 'EUR': 0.87, 'PKR': 279.13}
+    rates = {'USD': 1.0, 'EUR': 0.87, 'PKR': 279.13, 'INR': 82.89}
     
     # Convert the amount to USD first
     usd_amount = amount / rates[from_currency]
@@ -16,8 +16,8 @@ def main():
     st.title("Currency Converter")
     
     amount = st.number_input("Enter amount:", min_value=0.01, step=0.01)
-    from_currency = st.selectbox("From Currency:", options=['USD', 'EUR', 'PKR'])
-    to_currency = st.selectbox("To Currency:", options=['USD', 'EUR', 'PKR'])
+    from_currency = st.selectbox("From Currency:", options=['USD', 'EUR', 'PKR', 'INR'])
+    to_currency = st.selectbox("To Currency:", options=['USD', 'EUR', 'PKR', 'INR'])
     
     if st.button("Convert"):
         converted_amount = currency_converter(amount, from_currency, to_currency)
